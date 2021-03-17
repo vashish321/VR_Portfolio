ce207e15f7e9c84b46c820acbd00fd8c13e7e99c
vashish321 <63172589+vashish321@users.noreply.github.com>
2021-03-17 14:10:34 -0400

    Update 17/03/2021

--- script/showcase.js
+++ script/showcase.js
@@ -37,16 +37,16 @@ function first_call()
 	{
 		
 		image_holder.setAttribute("style", "background: url(image/1.jpg) no-repeat center center / cover;");
-		project_title.innerHTML = "Item1";
-		project_des.innerHTML = "this is item1";
+		project_title.innerHTML = "Charles Street Video";
+		project_des.innerHTML = "This website was aimed to redesign the website of an organisation whose cause was charitable. This project included the redesign of the website and mobile versions of the website and ensuring it was appealing to all the present and future stakeholders of the organization. Prototyping included using paper wireframes and adding clickable functions to them in order to iterate possible solutions to the problem. Hi-level iterations of prototypes included using figma and high level graphics to animate the webiste and deeply disseminate the functionality of the website and mobile application.";
 		document.getElementById("set_pdf").src = "pdf/1.pdf";
 		
 	}
 	else if(slider_counter == 2)
 	{
 		image_holder.setAttribute("style", "background: url(image/2.jpg) no-repeat center center / cover;");
-		project_title.innerHTML = "Item2";
-		project_des.innerHTML = "this is item2";
+		project_title.innerHTML = "Genius Create";
+		project_des.innerHTML = "This project included the design of the features of the app as well as the onboarding process demonstrated using Figma. The user research was a structured approach where the user persona was developed and the key characteristics to developing the needs and pain points of the user were defined. In conclusion, only the mobile version of the app was developed and the project was a success in detailing its main features. Further iterations of this design would include more user research to target the key demographics as well as a website further of the application.";
 		document.getElementById("set_pdf").src = "pdf/2.pdf";
 		
 		
@@ -69,8 +69,8 @@ function first_call()
 	{
 		
 		image_holder.setAttribute("style", "background: url(image/4.jpg) no-repeat center center / cover;");
-		project_title.innerHTML = "Item4";
-		project_des.innerHTML = "this is item4";
+		project_title.innerHTML = "First UX UI Portfolio";
+		project_des.innerHTML = "This is a simple presentation featuring one of my first UX UI Portfolios. It can be used in lieu of going through this entire website.";
 		document.getElementById("set_pdf").src = "pdf/4.pdf";
 		
 		
